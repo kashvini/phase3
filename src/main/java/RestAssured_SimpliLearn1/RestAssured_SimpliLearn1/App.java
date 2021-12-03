663f5bbf5e9753790d77abcd6c33c6138486e46a
kashvini <kunchalashvini@gmail.com>
2021-12-03 17:33:58 +0000

    postman

---
+++ src/main/java/RestAssured_SimpliLearn1/RestAssured_SimpliLearn1/App.java
@@ -0,0 +1,13 @@
+package RestAssured_SimpliLearn1.RestAssured_SimpliLearn1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
